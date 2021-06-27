ad9be5ca035b094bb0b4d6fd5e4be22e1c37e1a4
pythonyalfa <industry_leader99@hotmail.com>
2021-06-26 23:51:17 -0700

    Just made these changes , don't know if im going to make it.

--- hw06-task04-talfaro.py
+++ hw06-task04-talfaro.py
@@ -1,4 +1,12 @@
 # Write a Python script that uses list comprehension to greet each animal in the list.
 # Input Strng : dog, cat, bear, fossa, tapir
+import time
 
+input_string = ['dog', 'cat', 'bear', 'fossa', 'tapir']
 
+comprehend = list(input_string)
+
+for i in comprehend:
+    print("Hello ",i.capitalize())
+    time.sleep(.70)
+print("Goodbye!")
